2fa79d78689e3a3b8197ff37ba0ae21c6d0988c5
Samuliak <samuliak77@gmail.com>
2024-11-23 12:26:08 +0100

    initial commit

---
+++ Flask/Flask/NSApplication.m
@@ -0,0 +1,12 @@
+//
+//  NSApplication.m
+//  Flask
+//
+//  Created by Samuliak on 23/11/2024.
+//
+
+#import <AppKit/AppKit.h>
+
+int NSApplicarionMain(int argc, const char* _Nonnull argv[_Nonnull]) {
+    return 0;
+}
