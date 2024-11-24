3a91cdef42138c7a679c99b00a3c577642b6cf30
Samuliak <samuliak77@gmail.com>
2024-11-24 17:27:24 +0100

    set app delegate create callback

--- Flask/NSApplication.m
+++ Flask/NSApplication.m
@@ -7,6 +7,14 @@
 
 #include "Common.h"
 
+id _Nonnull defaultAppDelegateCreateCallback(TqlObject* _Nonnull obj) {
+    id appDelegate = [[NSClassFromString(@"AppDelegate") alloc] init];
+    
+    return appDelegate;
+}
+
 extern int NSApplicationMain(int argc, const char* _Nonnull argv[_Nonnull]) {
+    tqlSetDefaultAppDelegateCreateCallback(defaultAppDelegateCreateCallback);
+    
     return tqlApplicarionMain(argc, argv);
 }
