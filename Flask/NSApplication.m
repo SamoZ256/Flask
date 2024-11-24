e9a6b4e28fbf9af3281f37a2f6fa611a3f681b4b
Samuliak <samuliak77@gmail.com>
2024-11-24 17:43:38 +0100

    implement application did finish launching callback

--- Flask/NSApplication.m
+++ Flask/NSApplication.m
@@ -13,8 +13,14 @@ id _Nonnull defaultAppDelegateCreateCallback(TqlObject* _Nonnull obj) {
     return appDelegate;
 }
 
+void applicationDidFinishLaunching(TqlObject* _Nonnull obj) {
+    // TODO: don't pass NULL
+    [obj->host applicationDidFinishLaunching:NULL];
+}
+
 extern int NSApplicationMain(int argc, const char* _Nonnull argv[_Nonnull]) {
     tqlSetDefaultAppDelegateCreateCallback(defaultAppDelegateCreateCallback);
+    tqlSetApplicationDidFinishLaunchingCallback(applicationDidFinishLaunching);
     
     return tqlApplicarionMain(argc, argv);
 }
