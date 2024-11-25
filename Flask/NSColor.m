6378c4d609923434e80c9761709da13d5c93bb24
Samuliak <samuliak77@gmail.com>
2024-11-25 16:56:34 +0100

    implement color

---
+++ Flask/NSColor.m
@@ -0,0 +1,26 @@
+//
+//  NSColor.m
+//  Flask
+//
+//  Created by Samuliak on 25/11/2024.
+//
+
+#include "Common.h"
+
+static NSColor* __yellowColor = nil;
+
+@implementation NSColor {
+    TqlObject* obj;
+}
+
++ (NSColor*)yellowColor {
+    if (!__yellowColor) {
+        __yellowColor = [[NSColor alloc] init];
+        __yellowColor->obj = tqlObjectCreate(__yellowColor);
+        tqlColorYellowCreate(__yellowColor->obj);
+    }
+    
+    return __yellowColor;
+}
+
+@end
