c9864fefe41aa1ecee8108206b21ff1546bb6053
Samuliak <samuliak77@gmail.com>
2024-12-03 17:29:44 +0100

    implement CGWarpMouseCursorPosition

---
+++ Flask/CoreGraphics/CGRemoteOperation.m
@@ -0,0 +1,16 @@
+//
+//  CGRemoteOperation.m
+//  Flask
+//
+//  Created by Samuliak on 03/12/2024.
+//
+
+#import <CoreGraphics/CoreGraphics.h>
+
+#include "Common.h"
+
+CGError CGWarpMouseCursorPosition(CGPoint newCursorPosition) {
+    // There is no direct equivalent on iOS
+    
+    return kCGErrorSuccess;
+}
