e1708887ac63e35869163a62a871bb81c882089c
Samuliak <samuliak77@gmail.com>
2024-12-03 17:36:59 +0100

    add: a base for NSEvent

---
+++ Flask/AppKit/NSEvent.m
@@ -0,0 +1,19 @@
+//
+//  NSEvent.m
+//  Flask
+//
+//  Created by Samuliak on 03/12/2024.
+//
+
+#import <AppKit/AppKit.h>
+
+#include "Common.h"
+
+@implementation NSEvent
+
++ (id)addLocalMonitorForEventsMatchingMask:(NSEventMask)mask handler:(NSEvent* _Nullable (^)(NSEvent* _Nonnull))block {
+    // TODO: implement
+    return nil;
+}
+
+@end
